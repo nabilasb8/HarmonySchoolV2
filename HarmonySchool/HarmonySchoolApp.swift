89e29306f3b58e24a419905f6929af171e0b1339
nabilasb8 <nabilasyafrina@icloud.com>
2022-08-18 17:21:06 +0700

    Initial commit

---
+++ HarmonySchool/HarmonySchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarmonySchoolApp.swift
+//  HarmonySchool
+//
+//  Created by Nabila on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct HarmonySchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
